e2bd29560a4e361244c2e3063f3b433ecd940981
narendrababu01 <106644585+narendrababu01@users.noreply.github.com>
2026-04-02 16:45:19 +0530

    Update main.java

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main{
 public static void main(String[] args) {
 System.out.println("Welcom to java first program");
   System.out.println("Welcom to java first program1");
+  System.out.println("Welcom to java first program2");
 }
 }
